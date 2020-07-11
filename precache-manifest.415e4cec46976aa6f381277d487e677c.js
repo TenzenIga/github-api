9cfa18f256d39fd448549a6c122b039b0a9a0bd5
TenzenIga <arhan-93@list.ru>
2020-07-11 15:18:38 +0300

    Updates

---
+++ precache-manifest.415e4cec46976aa6f381277d487e677c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534f153b84aa262816b35834bfbf6cbc",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "fbad9e24900b8c62ad4c",
+    "url": "/github-api/static/css/2.12f1a8df.chunk.css"
+  },
+  {
+    "revision": "61d8a2b91f81c18109b0",
+    "url": "/github-api/static/css/main.3c6c6e2a.chunk.css"
+  },
+  {
+    "revision": "fbad9e24900b8c62ad4c",
+    "url": "/github-api/static/js/2.72505d24.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/github-api/static/js/2.72505d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d8a2b91f81c18109b0",
+    "url": "/github-api/static/js/main.8355bb99.chunk.js"
+  },
+  {
+    "revision": "e0449004630b533b17f9",
+    "url": "/github-api/static/js/runtime-main.6e609026.js"
+  }
+]);
